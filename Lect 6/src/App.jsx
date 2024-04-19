ae9f3539de707ee277871dd5a26b081dc4614bbe
Chirag-Devda <devdachirag09@gmail.com>
2024-04-19 16:19:59 +0530

    modify

--- Lect 6/src/App.jsx
+++ Lect 6/src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useMemo } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+var nums = new Array(30_000_000).fill(0).map((_, i) => {
+  return {
+    item: i,
+    ismagical: i === 29_000_000,
+  };
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [numbers, setnumbers] = useState(nums);
+
+  // const magical = numbers.find((item) => item.ismagical === true); // Expensive computation this will re-render when there is change in any state
+
+  const magical = useMemo(
+    () => numbers.find((item) => item.ismagical === true),
+    [numbers]
+  ); // this will find the result one time and save it and usememo will make website smooth . but if there is any change in dependece this will run again and save the result after these again run smooth
 
   return (
     <>
       <div>
+        <span>Magical number is {magical?.item}</span>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -18,7 +34,21 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() => {
+            setCount((count) => count + 1);
+            if (count === 10) {
+              setnumbers(
+                new Array(10_000_000).fill(0).map((_, i) => {
+                  return {
+                    item: i,
+                    ismagical: i === 9_000_000,
+                  };
+                })
+              );
+            }
+          }}
+        >
           count is {count}
         </button>
         <p>
@@ -29,7 +59,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
