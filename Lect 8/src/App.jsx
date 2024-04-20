ef19e8b24affbd5ba1f10bd26478c195d9b5844e
Chirag-Devda <devdachirag09@gmail.com>
2024-04-20 22:16:55 +0530

    Practiced Form Handling
    Form handling using React Hook Form practiced basic functions like register , handleSubmit , setError , formState: { errors, isSubmitting },

---
+++ Lect 8/src/App.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useForm } from "react-hook-form";
+function App() {
+  const [count, setCount] = useState(0);
+
+  const delay = (d) => {
+    let p = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("hello");
+      }, d * 1000);
+    });
+    return p;
+  };
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm();
+  const onSubmit = async (data) => {
+    await delay(2);
+    console.log(data);
+    if (data.UserName === "sahil") {
+      setError("invaliduser", { message: "this user is cheap" });
+    }
+  };
+  return (
+    <>
+      {isSubmitting && (
+        <div>
+          {isSubmitting && (
+            <div className="loader-container">
+              <div className="loader"></div>
+              <div className="loading-text">Loading...</div>
+            </div>
+          )}
+        </div>
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          {...register("UserName", {
+            required: { value: true, message: "*" },
+            minLength: { value: 2, message: " ! Minumum length is 2" },
+            maxLength: { value: 10, message: " ! Maxumum length is 10" },
+          })}
+          placeholder="Username"
+        />
+        {errors.UserName && (
+          <>
+            <span className="red">{errors.UserName.message}</span>
+          </>
+        )}
+        <br />
+        <br />
+        <input
+          type="password"
+          {...register("Password", {
+            required: { value: true, message: "*" },
+            minLength: { value: 8, message: "minimum 8 length" },
+            maxLength: { value: 10, message: "maximum 10 length" },
+          })}
+          placeholder="Password....."
+        />
+        {errors.Password && (
+          <>
+            <span className="red">{errors.Password.message}</span>
+          </>
+        )}
+        <br />
+        <br />
+        <input disabled={isSubmitting} type="submit" value="submit" />
+        {errors.invaliduser && (
+          <>
+            <br />
+            <div className="red">{errors.invaliduser.message}</div>
+          </>
+        )}
+      </form>
+    </>
+  );
+}
+
+export default App;
